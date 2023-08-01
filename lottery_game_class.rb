4ec4583977c6f93640dc9c726dac995c0059df6b
Alexssander Tavares <alexssander.santos2@gmail.com>
2023-08-01 00:01:06 -0300

    fix: missing method definition fixed  On branch main  Changes to be committed: 	modified:   lottery_game_class.rb

--- lottery_game_class.rb
+++ lottery_game_class.rb
@@ -48,5 +48,52 @@ class LotteryGame
             puts "Wrong entry"
         end
     end
-end
 
+    def game_init
+        puts "Welcome to 'What number combination I am thinking game'"
+        puts "\n\n"
+        puts "Very well, now choose four numbers and try your luck!!!\n\n"
+        user_entry = Array.new
+        print "First number: "
+        user_entry[0] = gets.chomp.to_i
+        print "Second number: "
+        user_entry[1] = gets.chomp.to_i
+        print "Third number: "
+        user_entry[2] = gets.chomp.to_i
+        print "Fourth number: "
+        user_entry[3] = gets.chomp.to_i
+
+        sort_display = Array.new
+        sort_display[0] = SecureRandom.random_number(9)
+        sort_display[1] = SecureRandom.random_number(9)
+        sort_display[2] = SecureRandom.random_number(9)
+        sort_display[3] = SecureRandom.random_number(9)
+
+        puts "Let's see if you got me..."
+
+        verify1 = user_entry[0] == sort_display[0]
+        verify2 = user_entry[1] == sort_display[1]
+        verify3 = user_entry[2] == sort_display[2]
+        verify4 = user_entry[3] == sort_display[3]
+
+        result = verify1.eql?(true) && verify2.eql?(true) && verify3.eql?(true) && verify4.eql?(true)
+        if result == true
+            puts "My number is: |#{sort_display[0]}|#{sort_display[1]}|#{sort_display[2]}|#{sort_display[3]}|"
+            puts "Congratulations!!!! You got me"
+        else
+            puts "My number is: |#{sort_display[0]}|#{sort_display[1]}|#{sort_display[2]}|#{sort_display[3]}|"
+            puts "You lost, do your best next time.\n\n"
+        end
+        
+        print "Do you want to try again? y/n "
+        answer = gets.chomp.to_s
+
+        if answer == "n" || answer == "no" || answer == "No" || answer == "NO"
+            return if answer == "n" || answer == "no" || answer == "No" || answer == "NO"
+        elsif answer == "y" || answer == "yes" || answer == "Yes" || answer == "YES"
+            self.game_init
+        else
+            puts "Wrong entry"
+        end
+    end
+end
